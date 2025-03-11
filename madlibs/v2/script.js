98504bbaa94faaf534e76bf03d82e9e3e282f9cc
Sammy Zhang <sammyzhang@campus-048-235.ucdavis.edu>
2025-03-11 15:58:25 -0700

    starting html for game

---
+++ madlibs/v2/script.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+    console.log('reading js');
+
+    const form = document.querySelector('#madlibs-form');
+    const overlay = document.querySelector('#story-overlay');
+    const story = document.querySelector('#story')
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // form values
+        const adjective = document.querySelector('#adjective').value;
+        const object = document.querySelector('#object').value;
+        const verb = document.querySelector('#verb').value;
+        const name = document.querySelector('#name').value;
+        const time = document.querySelector('#time').value;
+        const weapon = document.querySelector('#weapon').value;
+        const direction = document.querySelector('#direction').value;
+
+        const madlib = `Your mission is to steal the <span class='highlight'>${adjective}</span> casino’s <span class='highlight'>${object}</span>, but first you need to <span class='highlight'>${verb}</span> past the security. Disguise yourself as <span class='highlight'>${name}</span> and join the high-stakes poker game. When the clock strikes <span class='highlight'>${time}</span>, use the <span class='highlight'>${weapon}</span> to break into the vault and retrieve the <span class='highlight'>${object}</span>. Don’t forget to escape by slipping out through the <span class='highlight'>${direction}.</span>`;
+    })
+})
