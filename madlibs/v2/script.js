6832d71cb73b2072ed5e873a95c2d8efbe1f94d7
Sammy Zhang <sammyzhang@Sammys-MacBook-Pro-2.local>
2025-03-17 18:02:10 -0700

    madlibs

--- madlibs/v2/script.js
+++ madlibs/v2/script.js
@@ -5,6 +5,8 @@
     const form = document.querySelector('#madlibs-form');
     const overlay = document.querySelector('#story-overlay');
     const story = document.querySelector('#story')
+    const closeBtn = document.querySelector('.close-btn');
+
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -18,6 +20,17 @@
         const weapon = document.querySelector('#weapon').value;
         const direction = document.querySelector('#direction').value;
 
-        const madlib = `Your mission is to steal the <span class='highlight'>${adjective}</span> casino’s <span class='highlight'>${object}</span>, but first you need to <span class='highlight'>${verb}</span> past the security. Disguise yourself as <span class='highlight'>${name}</span> and join the high-stakes poker game. When the clock strikes <span class='highlight'>${time}</span>, use the <span class='highlight'>${weapon}</span> to break into the vault and retrieve the <span class='highlight'>${object}</span>. Don’t forget to escape by slipping out through the <span class='highlight'>${direction}.</span>`;
+        const madlib = `Your mission is to steal the <span class='highlight'>${adjective}</span> casino's <span class='highlight'>${object}</span>, but first you need to <span class='highlight'>${verb}</span> past the security. Disguise yourself as <span class='highlight'>${name}</span> and join the high-stakes poker game. When the clock strikes <span class='highlight'>${time}</span>, use the <span class='highlight'>${weapon}</span> to break into the vault and retrieve the <span class='highlight'>${object}</span>. Don't forget to escape by slipping out through the <span class='highlight'>${direction}</span>.`;
+
+        story.innerHTML = madlib;
+        overlay.classList.remove('hidden');
     })
-})
+
+    overlay.addEventListener('click', function() {
+        overlay.classList.add('hidden');
+    });
+
+    closeBtn.addEventListener('click', function() {
+        overlay.classList.add('hidden');
+    });
+})();
